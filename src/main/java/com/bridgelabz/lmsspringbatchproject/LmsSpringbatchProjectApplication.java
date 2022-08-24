94d69cadb80f19208d9a5fadbdf292e8973577f2
Qa-krushna <rashwini510@gmail.com>
2022-08-24 15:52:52 +0530

    Added: Welcome to spring batch project

---
+++ src/main/java/com/bridgelabz/lmsspringbatchproject/LmsSpringbatchProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lmsspringbatchproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsSpringbatchProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmsSpringbatchProjectApplication.class, args);
+    }
+
+}
